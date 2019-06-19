dfda668dfce503958e9c637dd1e1a71b0234f5cc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-06-18 19:10:34 -0700

    proxy ec2

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-app.use('/api/stocks/', proxy('http://localhost:3111/api/stocks/'));
-app.use('/api/increments/', proxy('http://localhost:3111/api/increments/'));
+app.use('/api/stocks/', proxy('http://ec2-18-222-193-177.us-east-2.compute.amazonaws.com:3111/api/stocks/'));
+app.use('/api/increments/', proxy('http://ec2-18-222-193-177.us-east-2.compute.amazonaws.com:3111/api/increments/'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
